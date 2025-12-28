a75cad4dd7fcdab9d212676229e5766512fa0833
Ishanxpun <punishan500@gmail.com>
2025-12-29 02:48:43 +0545

    herosection text added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,29 @@
 import Nav from "@/components/Nav";
 import React from "react";
 import { Inter } from "next/font/google";
+import { Geist,Geist_Mono } from "next/font/google";
+import { Jersey_10, Russo_One } from "next/font/google";
+
+const geistS = Geist({
+  weight:"600",
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+const geistM = Geist_Mono({
+  weight:"900",
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+const jersey = Jersey_10({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+const russo = Russo_One({
+  weight: "400",
+  subsets: ["latin"],
+});
+
 const page = () => {
   return (
    
@@ -11,9 +34,9 @@ const page = () => {
           src="/videos/animegirl.mp4"
           className="object-cover w-full h-full"
         ></video>
-        <div className="absolute z-40 bottom-10 left-5"> 
-         <div className=" text-white  font-In text-5xl flex gap-3">A <div className="text-red-400">Portal</div> to <div className="text-red-400">Escape Reality</div></div>  
-          <div className="text-white text-2xl">“A World Waiting to Be Explored"</div>
+        <div className={`absolute z-40 bottom-30 left-8 ${russo.className} `}> 
+         <div className=" text-white/70   text-5xl flex gap-4">A <div className="text-white/70">Portal</div> to <div className="text-red-400">Escape Reality</div></div>  
+          <div className="text-white/40 text-2xl">“A World Waiting to Be Explored"</div>
           </div>
         
       </div>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
+
 const Nav = () => {
   return (
     <div className="relative z-40 mx-6 ">
-      <nav className=" rounded-4xl px-6 absolute w-full flex justify-between text-white text-xl font-light  py-2 bg-white/1 backdrop-blur-sm  shadow-[2px_2px_5px_2px_rgba(0,0,0,0.3)] mt-4">
+      <nav className=" rounded-2xl px-6 absolute w-full flex justify-between text-white text-xl font-light  py-2 bg-white/1 backdrop-blur-sm  shadow-[2px_2px_5px_2px_rgba(0,0,0,0.3)] mt-3">
         <div className="Left">
           <Image src="/logo.png" alt="img" height={50} width={80} className="hover:scale-110 opacity-80"/>
         </div>
-        <div className="midpart flex gap-6  pl-55">
+        <div className="midpart flex gap-6  pl-50 ">
           <Link href={"/"} className="hover:scale-110 ">Home</Link>
           <Link href={"/anime "} className="hover:scale-110">Anime</Link>
           <Link href={"/movies"} className="hover:scale-110">Movies</Link>
@@ -21,7 +22,7 @@ const Nav = () => {
           <input
             type="text"
             placeholder="   Search "
-            className="border rounded-md shadow inline-flex cursor-pointer opacity-70 bg-black/20 "
+            className=" border  rounded-md shadow inline-flex cursor-pointer opacity-80 bg-black/20 "
           />
            <div className=" inline p-1 rounded-md absolute bottom-1 right-1 opacity-70  " >🔍︎</div>
           </div>
