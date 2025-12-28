1857e7f7c161fbcc95de3b13258a04e0cc623ce2
Ishanxpun <punishan500@gmail.com>
2025-12-29 01:39:26 +0545

    nav done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,25 @@
-
-import React from 'react'
-
+import Nav from "@/components/Nav";
+import React from "react";
+import { Inter } from "next/font/google";
 const page = () => {
   return (
-    <div>
-  <div className='h-screen w-screen overflow-hidden relative'>
-            <video autoPlay muted src="/videos/animegirl.mp4" className='object-cover w-full h-full'></video>
-    </div>
-      
-    </div>
-  )
-}
+   
+      <div className="h-screen w-screen overflow-hidden relative ">
+        <video
+          autoPlay
+          muted
+          src="/videos/animegirl.mp4"
+          className="object-cover w-full h-full"
+        ></video>
+        <div className="absolute z-40 bottom-10 left-5"> 
+         <div className=" text-white  font-In text-5xl flex gap-3">A <div className="text-red-400">Portal</div> to <div className="text-red-400">Escape Reality</div></div>  
+          <div className="text-white text-2xl">“A World Waiting to Be Explored"</div>
+          </div>
+        
+      </div>
+     
+    
+  );
+};
 
-export default page
+export default page;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,28 +1,36 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
 const Nav = () => {
   return (
-   <nav className='h-[10px] absolute w-screen z-2 flex justify-between text-white text-xl font-light'>
-        <div className='Left'>
-         <Image src="/logo.png" alt="img" height={50} width={80}/>
+    <div className="relative z-40 mx-6 ">
+      <nav className=" rounded-4xl px-6 absolute w-full flex justify-between text-white text-xl font-light  py-2 bg-white/1 backdrop-blur-sm  shadow-[2px_2px_5px_2px_rgba(0,0,0,0.3)] mt-4">
+        <div className="Left">
+          <Image src="/logo.png" alt="img" height={50} width={80} className="hover:scale-110 opacity-80"/>
         </div>
-        <div className='midpart flex gap-6'>
-           <Link href={"/"}>Home</Link>
-           <Link href={"/anime"}>Anime</Link>
-           <Link href={"/movies"}>Movies</Link>
-           <Link href={"/popular"}>Popular</Link>
-           <Link href={"/browse"}>Browse</Link>
+        <div className="midpart flex gap-6  pl-55">
+          <Link href={"/"} className="hover:scale-110 ">Home</Link>
+          <Link href={"/anime "} className="hover:scale-110">Anime</Link>
+          <Link href={"/movies"} className="hover:scale-110">Movies</Link>
+          <Link href={"/popular"} className="hover:scale-110">Popular</Link>
+          <Link href={"/browse"} className="hover:scale-110">Browse</Link>
         </div>
-        <div className='right'>
-             search
+        <div className="right flex">
+          <div className="relative"> 
+          <input
+            type="text"
+            placeholder="   Search "
+            className="border rounded-md shadow inline-flex cursor-pointer opacity-70 bg-black/20 "
+          />
+           <div className=" inline p-1 rounded-md absolute bottom-1 right-1 opacity-70  " >🔍︎</div>
+          </div>
+         
+         
         </div>
-  
-    </nav>
+      </nav>
+    </div>
+  );
+};
 
-  
-  )
-}
-
-export default Nav
+export default Nav;
